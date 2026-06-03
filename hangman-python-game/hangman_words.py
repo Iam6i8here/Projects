0dd45c3dacd454ea7562a24b21dec66e91819b85
Anukrishna V <iam6i8here@gmail.com>
2026-06-03 22:20:33 +0530

    Add files via upload

---
+++ hangman-python-game/hangman_words.py
@@ -0,0 +1,9 @@
+# hangman_words.py
+
+word_list = [
+    "aardvark", "baboon", "camel", "elephant", "giraffe", "kangaroo",
+    "alligator", "antelope", "buffalo", "cheetah", "chimpanzee",
+    "crocodile", "dolphin", "eagle", "falcon", "gorilla", "hippopotamus",
+    "iguana", "jaguar", "koala", "lemur", "mongoose", "octopus",
+    "penguin", "rhinoceros", "squirrel", "tiger", "walrus", "zebra"
+]
